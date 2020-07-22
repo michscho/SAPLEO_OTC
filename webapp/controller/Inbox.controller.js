aaeaef7d027f60b97876c3c947d2d3bd2ec22d16
Nolimimer <schottmichaelsw@gmail.com>
2020-07-22 14:13:31 +0000

    Update Upload Document

--- webapp/controller/Inbox.controller.js
+++ webapp/controller/Inbox.controller.js
@@ -1,268 +1,281 @@
-sap.ui.define(["sap/ui/core/mvc/Controller",
-	"sap/m/MessageBox",
-	"./utilities",
-	"sap/ui/core/routing/History"
-], function(BaseController, MessageBox, Utilities, History) {
-	"use strict";
-
-	return BaseController.extend("com.sap.build.standard.otcOptimization.controller.Inbox", {
-		handleRouteMatched: function(oEvent) {
-			var sAppId = "App5efc5c3a04d5c13a7d081e1e";
-
-			var oParams = {};
-			var oView = this.getView();
-			var bSelectFirstListItem = true;
-			if (oEvent.mParameters.data.context || oEvent.mParameters.data.masterContext) {
-				this.sContext = oEvent.mParameters.data.context;
-
-				this.sMasterContext = oEvent.mParameters.data.masterContext;
-
-			} else {
-				if (this.getOwnerComponent().getComponentData()) {
-					var patternConvert = function(oParam) {
-						if (Object.keys(oParam).length !== 0) {
-							for (var prop in oParam) {
-								if (prop !== "sourcePrototype" && prop.includes("Set")) {
-									return prop + "(" + oParam[prop][0] + ")";
+sap.ui.define([
+		"sap/ui/core/mvc/Controller",
+		"sap/m/MessageBox",
+		"./utilities",
+		"sap/ui/core/routing/History"
+	], function (BaseController, MessageBox, Utilities, History) {
+		"use strict";
+		return BaseController.extend("com.sap.build.standard.otcOptimization.controller.Inbox", {
+			handleRouteMatched: function (oEvent) {
+				var sAppId = "App5efc5c3a04d5c13a7d081e1e";
+				var oParams = {};
+				var oView = this.getView();
+				var bSelectFirstListItem = true;
+				if (oEvent.mParameters.data.context || oEvent.mParameters.data.masterContext) {
+					this.sContext = oEvent.mParameters.data.context;
+					this.sMasterContext = oEvent.mParameters.data.masterContext;
+				} else {
+					if (this.getOwnerComponent().getComponentData()) {
+						var patternConvert = function (oParam) {
+							if (Object.keys(oParam).length !== 0) {
+								for (var prop in oParam) {
+									if (prop !== "sourcePrototype" && prop.includes("Set")) {
+										return prop + "(" + oParam[prop][0] + ")";
+									}
 								}
 							}
-						}
-					};
-
-					this.sMasterContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
-
+						};
+						this.sMasterContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+					}
 				}
-			}
-
-			var oPath;
-
-			if (this.sMasterContext && oEvent.getParameters().config.bypassed.target[0] !== this.sMasterContext) {
-				oPath = {
-					path: "/" + this.sMasterContext,
-					parameters: oParams
-				};
-				this.getView().bindObject(oPath);
-			} else if (this.sContext) {
-				var sCurrentContextPath = "/" + this.sContext;
-
-				bSelectFirstListItem = false;
-			}
-
-			if (bSelectFirstListItem) {
-				oView.addEventDelegate({
-					onBeforeShow: function() {
-						var oContent = this.getView().getContent();
-						if (oContent) {
-							if (!sap.ui.Device.system.phone) {
-								var oList = oContent[0].getContent() ? oContent[0].getContent()[0] : undefined;
-								if (oList) {
-									var sContentName = oList.getMetadata().getName();
-									if (sContentName.indexOf("List") > -1) {
-										oList.attachEventOnce("updateFinished", function() {
-											var oFirstListItem = this.getItems()[0];
-											if (oFirstListItem) {
-												oList.setSelectedItem(oFirstListItem);
-												oList.fireItemPress({
-													listItem: oFirstListItem
-												});
-											}
-										}.bind(oList));
+				var oPath;
+				if (this.sMasterContext && oEvent.getParameters().config.bypassed.target[0] !== this.sMasterContext) {
+					oPath = {
+						path: "/" + this.sMasterContext,
+						parameters: oParams
+					};
+					this.getView().bindObject(oPath);
+				} else if (this.sContext) {
+					var sCurrentContextPath = "/" + this.sContext;
+					bSelectFirstListItem = false;
+				}
+				if (bSelectFirstListItem) {
+					oView.addEventDelegate({
+						onBeforeShow: function () {
+							var oContent = this.getView().getContent();
+							if (oContent) {
+								if (!sap.ui.Device.system.phone) {
+									var oList = oContent[0].getContent() ? oContent[0].getContent()[0] : undefined;
+									if (oList) {
+										var sContentName = oList.getMetadata().getName();
+										if (sContentName.indexOf("List") > -1) {
+											oList.attachEventOnce("updateFinished", function () {
+												var oFirstListItem = this.getItems()[0];
+												if (oFirstListItem) {
+													oList.setSelectedItem(oFirstListItem);
+													oList.fireItemPress({
+														listItem: oFirstListItem
+													});
+												}
+											}.bind(oList));
+										}
 									}
 								}
 							}
-						}
-					}.bind(this)
-				});
-			}
-
-		},
-		_attachSelectListItemWithContextPath: function(sContextPath) {
-			var oView = this.getView();
-			var oContent = this.getView().getContent();
-			if (oContent) {
-				if (!sap.ui.Device.system.phone) {
-					var oList = oContent[0].getContent() ? oContent[0].getContent()[0] : undefined;
-					if (oList && sContextPath) {
-						var sContentName = oList.getMetadata().getName();
-						var oItemToSelect, oItem, oContext, aItems, i;
-						if (sContentName.indexOf("List") > -1) {
-							if (oList.getItems().length) {
-								oItemToSelect = null;
-								aItems = oList.getItems();
-								for (i = 0; i < aItems.length; i++) {
-									oItem = aItems[i];
-									oContext = oItem.getBindingContext();
-									if (oContext && oContext.getPath() === sContextPath) {
-										oItemToSelect = oItem;
+						}.bind(this)
+					});
+				}
+			},
+			_attachSelectListItemWithContextPath: function (sContextPath) {
+				var oView = this.getView();
+				var oContent = this.getView().getContent();
+				if (oContent) {
+					if (!sap.ui.Device.system.phone) {
+						var oList = oContent[0].getContent() ? oContent[0].getContent()[0] : undefined;
+						if (oList && sContextPath) {
+							var sContentName = oList.getMetadata().getName();
+							var oItemToSelect, oItem, oContext, aItems, i;
+							if (sContentName.indexOf("List") > -1) {
+								if (oList.getItems().length) {
+									oItemToSelect = null;
+									aItems = oList.getItems();
+									for (i = 0; i < aItems.length; i++) {
+										oItem = aItems[i];
+										oContext = oItem.getBindingContext();
+										if (oContext && oContext.getPath() === sContextPath) {
+											oItemToSelect = oItem;
+										}
 									}
+									if (oItemToSelect) {
+										oList.setSelectedItem(oItemToSelect);
+									}
+								} else {
+									oView.addEventDelegate({
+										onBeforeShow: function () {
+											oList.attachEventOnce("updateFinished", function () {
+												oItemToSelect = null;
+												aItems = oList.getItems();
+												for (i = 0; i < aItems.length; i++) {
+													oItem = aItems[i];
+													oContext = oItem.getBindingContext();
+													if (oContext && oContext.getPath() === sContextPath) {
+														oItemToSelect = oItem;
+													}
+												}
+												if (oItemToSelect) {
+													oList.setSelectedItem(oItemToSelect);
+												}
+											});
+										}
+									});
 								}
-								if (oItemToSelect) {
-									oList.setSelectedItem(oItemToSelect);
+							}
+						}
+					}
+				}
+			},
+			_onButtonPress: function () {
+				var oView = this.getView(),
+					status = true,
+					requiredFieldInfo = [];
+				if (requiredFieldInfo.length) {
+					status = this.handleChangeValuestate(requiredFieldInfo, oView);
+				}
+				if (status) {
+					return new Promise(function (fnResolve, fnReject) {
+						var bHasPendingChanges = false;
+						var oModel;
+						oModel = this.getView().getModel();
+						bHasPendingChanges = oModel && oModel.hasPendingChanges();
+						if (bHasPendingChanges) {
+							var sUserMessage = "Please save your changes, first";
+							fnReject(new Error(sUserMessage));
+						} else {
+							var oNewEntityInstance = Utilities.getDefaultValuesForEditOverviewEmail();
+							oModel = this.getView().getModel();
+							var oNewBindingContext = oModel.createEntry("BASet", {
+								properties: oNewEntityInstance
+							});
+							this.doNavigate("EditOverviewEmail", oNewBindingContext, fnResolve);
+						}
+					}.bind(this)).catch(function (err) {
+						if (err !== undefined) {
+							MessageBox.error(err.message);
+						}
+					});
+				}
+			},
+			doNavigate: function (sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+				var sPath = oBindingContext ? oBindingContext.getPath() : null;
+				var oModel = oBindingContext ? oBindingContext.getModel() : null;
+				var sEntityNameSet;
+				if (sPath !== null && sPath !== "") {
+					if (sPath.substring(0, 1) === "/") {
+						sPath = sPath.substring(1);
+					}
+					sEntityNameSet = sPath.split("(")[0];
+				}
+				var sNavigationPropertyName;
+				var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+				if (sEntityNameSet !== null) {
+					sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet,
+						sRouteName);
+				}
+				if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+					if (sNavigationPropertyName === "") {
+						this.oRouter.navTo(sRouteName, {
+							context: sPath,
+							masterContext: sMasterContext
+						}, false);
+					} else {
+						oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function (bindingContext) {
+							if (bindingContext) {
+								sPath = bindingContext.getPath();
+								if (sPath.substring(0, 1) === "/") {
+									sPath = sPath.substring(1);
 								}
 							} else {
-								oView.addEventDelegate({
-									onBeforeShow: function() {
-										oList.attachEventOnce("updateFinished", function() {
-											oItemToSelect = null;
-											aItems = oList.getItems();
-											for (i = 0; i < aItems.length; i++) {
-												oItem = aItems[i];
-												oContext = oItem.getBindingContext();
-												if (oContext && oContext.getPath() === sContextPath) {
-													oItemToSelect = oItem;
-												}
-											}
-											if (oItemToSelect) {
-												oList.setSelectedItem(oItemToSelect);
-											}
-										});
-									}
-								});
+								sPath = "undefined";
 							}
-						}
-
+							// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+							if (sPath === "undefined") {
+								this.oRouter.navTo(sRouteName);
+							} else {
+								this.oRouter.navTo(sRouteName, {
+									context: sPath,
+									masterContext: sMasterContext
+								}, false);
+							}
+						}.bind(this));
 					}
-				}
-			}
-
-		},
-		_onButtonPress: function() {
-			var oView = this.getView(),
-				status = true,
-				requiredFieldInfo = [];
-			if (requiredFieldInfo.length) {
-				status = this.handleChangeValuestate(requiredFieldInfo, oView);
-			}
-			if (status) {
-
-				return new Promise(function(fnResolve, fnReject) {
-					var bHasPendingChanges = false;
-					var oModel;
-
-					oModel = this.getView().getModel();
-					bHasPendingChanges = oModel && oModel.hasPendingChanges();
-
-					if (bHasPendingChanges) {
-						var sUserMessage = "Please save your changes, first";
-						fnReject(new Error(sUserMessage));
-					} else {
-						var oNewEntityInstance = Utilities.getDefaultValuesForEditOverviewEmail();
-
-						oModel = this.getView().getModel();
-						var oNewBindingContext = oModel.createEntry("BASet", {
-							properties: oNewEntityInstance
-						});
-
-						this.doNavigate("EditOverviewEmail", oNewBindingContext, fnResolve);
-					}
-				}.bind(this)).catch(function(err) {
-					if (err !== undefined) {
-						MessageBox.error(err.message);
-					}
-				});
-			}
-
-		},
-		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
-			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
-			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
-
-			var sEntityNameSet;
-			if (sPath !== null && sPath !== "") {
-				if (sPath.substring(0, 1) === "/") {
-					sPath = sPath.substring(1);
-				}
-				sEntityNameSet = sPath.split("(")[0];
-			}
-			var sNavigationPropertyName;
-			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
-
-			if (sEntityNameSet !== null) {
-				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
-			}
-			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
-				if (sNavigationPropertyName === "") {
-					this.oRouter.navTo(sRouteName, {
-						context: sPath,
-						masterContext: sMasterContext
-					}, false);
 				} else {
-					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
-						if (bindingContext) {
-							sPath = bindingContext.getPath();
-							if (sPath.substring(0, 1) === "/") {
-								sPath = sPath.substring(1);
-							}
-						} else {
-							sPath = "undefined";
-						}
-
-						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
-						if (sPath === "undefined") {
-							this.oRouter.navTo(sRouteName);
-						} else {
-							this.oRouter.navTo(sRouteName, {
-								context: sPath,
-								masterContext: sMasterContext
-							}, false);
-						}
-					}.bind(this));
+					this.oRouter.navTo(sRouteName);
 				}
-			} else {
-				this.oRouter.navTo(sRouteName);
-			}
-
-			if (typeof fnPromiseResolve === "function") {
-				fnPromiseResolve();
-			}
-
-		},
-		handleChangeValuestate: function(requiredFieldInfo, oView) {
-			var status = true;
-			if (requiredFieldInfo) {
-				requiredFieldInfo.forEach(function(requiredinfo) {
-					var input = oView.byId(requiredinfo.id);
-					if (input) {
-						input.setValueState("None"); //initially set ValueState to None
-						if (input.getValue() === '') {
-							input.setValueState("Error"); //input is blank set ValueState to error
-							status = false;
-						} else if (input.getDateValue && !input._bValid) { //since 1.64 ui5 will be providing a function 'isValidValue' that can be used here.
-							input.setValueState("Error"); //Invalid Date set ValueState to error
-							status = false;
+				if (typeof fnPromiseResolve === "function") {
+					fnPromiseResolve();
+				}
+			},
+			handleChangeValuestate: function (requiredFieldInfo, oView) {
+				var status = true;
+				if (requiredFieldInfo) {
+					requiredFieldInfo.forEach(function (requiredinfo) {
+						var input = oView.byId(requiredinfo.id);
+						if (input) {
+							input.setValueState("None");
+							//initially set ValueState to None
+							if (input.getValue() === "") {
+								input.setValueState("Error");
+								//input is blank set ValueState to error
+								status = false;
+							} else if (input.getDateValue && !input._bValid) {
+								//since 1.64 ui5 will be providing a function 'isValidValue' that can be used here.
+								input.setValueState("Error");
+								//Invalid Date set ValueState to error
+								status = false;
+							}
 						}
+					});
+				}
+				return status;
+			},
+			onInit: function () {
+				this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				this.oRouter.getTarget("Inbox").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			},
+			onExit: function () {
+				// to destroy templates for bound aggregations when templateShareable is true on exit to prevent duplicateId issue
+				var aControls = [{
+					"controlId": "sap_List_Page_0-content-sap_m_ObjectList-1527637257161",
+					"groups": ["items"]
+				}];
+				for (var i = 0; i < aControls.length; i++) {
+					var oControl = this.getView().byId(aControls[i].controlId);
+					if (oControl) {
+						for (var j = 0; j < aControls[i].groups.length; j++) {
+							var sAggregationName = aControls[i].groups[j];
+							var oBindingInfo = oControl.getBindingInfo(sAggregationName);
+							if (oBindingInfo) {
+								var oTemplate = oBindingInfo.template;
+								oTemplate.destroy();
+							}
+						}
+					}
+				}
+			},
+			/**
+			 *@memberOf com.sap.build.standard.otcOptimization.controller.Inbox
+			 */
+			action: function (oEvent) {
+				var that = this;
+				var actionParameters = JSON.parse(oEvent.getSource().data("wiring").replace(/'/g, "\""));
+				var eventType = oEvent.getId();
+				var aTargets = actionParameters[eventType].targets || [];
+				aTargets.forEach(function (oTarget) {
+					var oControl = that.byId(oTarget.id);
+					if (oControl) {
+						var oParams = {};
+						for (var prop in oTarget.parameters) {
+							oParams[prop] = oEvent.getParameter(oTarget.parameters[prop]);
+						}
+						oControl[oTarget.action](oParams);
 					}
 				});
-			}
-			return status;
-
-		},
-		onInit: function() {
-			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			this.oRouter.getTarget("Inbox").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
-
-		},
-		onExit: function() {
-
-			// to destroy templates for bound aggregations when templateShareable is true on exit to prevent duplicateId issue
-			var aControls = [{
-				"controlId": "sap_List_Page_0-content-sap_m_ObjectList-1527637257161",
-				"groups": ["items"]
-			}];
-			for (var i = 0; i < aControls.length; i++) {
-				var oControl = this.getView().byId(aControls[i].controlId);
-				if (oControl) {
-					for (var j = 0; j < aControls[i].groups.length; j++) {
-						var sAggregationName = aControls[i].groups[j];
-						var oBindingInfo = oControl.getBindingInfo(sAggregationName);
-						if (oBindingInfo) {
-							var oTemplate = oBindingInfo.template;
-							oTemplate.destroy();
-						}
+				var oNavigation = actionParameters[eventType].navigation;
+				if (oNavigation) {
+					var oParams = {};
+					(oNavigation.keys || []).forEach(function (prop) {
+						oParams[prop.name] = encodeURIComponent(JSON.stringify({
+							value: oEvent.getSource().getBindingContext(oNavigation.model).getProperty(prop.name),
+							type: prop.type
+						}));
+					});
+					if (Object.getOwnPropertyNames(oParams).length !== 0) {
+						this.getOwnerComponent().getRouter().navTo(oNavigation.routeName, oParams);
+					} else {
+						this.getOwnerComponent().getRouter().navTo(oNavigation.routeName);
 					}
 				}
 			}
-
-		}
-	});
-}, /* bExport= */ true);
+		});
+	}, /* bExport= */
+	true);
